4b4b512e062a6502d881217c5859473ad38b3ce7
slimslman778-jpg <slimslman778@gmail.com>
2026-08-29 13:39:26 +0300

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,8 +1,16 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-import firebaseConfig from '../firebase-applet-config.json';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBghQoC0jjssi9qC1AtkjzeN7rnJEK8tF4",
+  authDomain: "alzurayqi-perfumes.firebaseapp.com",
+  projectId: "alzurayqi-perfumes",
+  storageBucket: "alzurayqi-perfumes.firebasestorage.app",
+  messagingSenderId: "109121768462",
+  appId: "1:109121768462:web:26615089e2dd48a6f7c05f"
+};
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
+export const db = getFirestore(app);
 export const auth = getAuth(app);
